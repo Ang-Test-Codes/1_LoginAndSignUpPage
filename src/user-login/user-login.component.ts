bf2b9ce246b95c3e19ff6032ab777d9db160921c
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2023-03-06 10:43:53 +0530

    Update Sign Up and refactor

--- src/user-login/user-login.component.ts
+++ src/user-login/user-login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-login',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginComponent implements OnInit {
 
+  signupUsers: any[] =[];
+  signupObj: any ={
+    username: '',
+    email: '',
+    password: ''
+  };
+  loginObj: any ={
+    username: '',
+    password: ''
+  };  
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSignUp(){
+    this.signupUsers.push(this.signupObj);
+    localStorage.setItem("signupUsers", JSON.stringify(this.signupUsers));
+    this.signupObj={
+      username: '',
+      email: '',
+      password: ''
+    };
+  }
+
+  onLogIn(){}
 }
